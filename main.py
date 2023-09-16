003a5c37cfcf3936f23eaa12541ba97b56948cd9
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-16 12:04:34 +0300

    added comments

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from flask import Flask
 
+# Create Flask application instance
 app = Flask(__name__)
 
-#route decorator -maps/route a url to the return value of the function
+# Define a route for the root URL '/'
 @app.route('/')
 def index():
     return 'This is the homepage'
 
+# Define a route for '/tuna'
 @app.route('/tuna')
 def tuna():
     return '<h2>Tuna is good </h2>'
 
+# Define a dynamic route with a placeholder variable 'username'
 @app.route('/profile/<username>')
 def profile(username):
+    # Extract the 'username' varibale from the URL and include it in the HTML response
     return '<h2>Hey there %s </h2>' %username
 
+# Check if the script is being run as the main program
 if __name__ == '__main__':
+    # Start flask application in debug mode
     app.run(debug=True)
