19e0d911cfbeb410a9b1b455460d7845bee96e6f
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-16 11:54:04 +0300

    returns a dynamic route with a placeholder variable 'username'

--- main.py
+++ main.py
@@ -11,5 +11,9 @@ def index():
 def tuna():
     return '<h2>Tuna is good </h2>'
 
+@app.route('/profile/<username>')
+def profile(username):
+    return '<h2>Hey there %s </h2>' %username
+
 if __name__ == '__main__':
     app.run(debug=True)
